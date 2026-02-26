43d9e25c9ec1ba18eeb4a6ab43b72be81e40fe0e
Edilson Escobar <leoedilson@gmail.com>
2026-02-26 22:58:50 +0100

    Transmision en vivo

---
+++ sw.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = 'winner-europa-v1';
+
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/trebol5.PNG'
+];
+
+// Instalación
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activación
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Fetch
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
