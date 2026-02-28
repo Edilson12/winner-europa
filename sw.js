75d087b409814eaccffb5a222e0811bd482bb898
Edilson Escobar <leoedilson@gmail.com>
2026-02-28 08:27:41 +0100

    Archivos corregidos para PWA Android

--- sw.js
+++ sw.js
@@ -1,41 +1,32 @@
-const CACHE_NAME = 'winner-europa-v8';
+const CACHE_NAME = 'winner-europa-v10';
 
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/trebol-192.png',
-  '/trebol-512.png'
+const ASSETS = [
+  '/winner-europa/',
+  '/winner-europa/index.html',
+  '/winner-europa/manifest.json',
+  '/winner-europa/trebol-192.png',
+  '/winner-europa/trebol-512.png',
+  '/winner-europa/fondo_winner.jpg'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cache => {
-          if (cache !== CACHE_NAME) {
-            return caches.delete(cache);
-          }
-        })
-      );
-    })
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))
+    )
   );
   self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(res => res || fetch(event.request))
   );
 });
